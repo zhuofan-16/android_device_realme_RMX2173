a7ff55c055db3710bdc974b8d78ee3725058e5fc
ZhuoFan <4582h@dunearn.edu.sg>
2020-12-22 17:17:36 +0000

    LOL,forgotten to decompile power_profile.xml & remove dupicate values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,6 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 include device/mediatek/sepolicy/sepolicy.mk
 
 -include vendor/realme/RMX2173/BoardConfigVendor.mk
